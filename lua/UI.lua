215144aa06687ee8377c2991a227534bce571b0e
lijie <lijie_1912@126.com>
2023-10-14 07:35:58 +0800

    first one linux

---
+++ lua/UI.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        event = "veryLazy",
+        config = true,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "VeryLazy",
+    },
+}
---
+++ lua/options.lua
@@ -0,0 +1,88 @@
+-- local define
+local o = vim.o
+local buffer = vim.b
+local global = vim.g
+
+o.cursorline = true
+
+-- number
+o.number = true
+o.relativenumber = true
+
+-- font
+o.showmode = false
+
+-- case 
+o.ignorecase = true
+o.smartcase = true
+
+o.scrolloff = 5
+-- menu
+o.wildmode = "full"
+
+-- extra file generate
+o.undofile = false
+o.swapfile = false
+o.backup = false
+
+-- tab behavier
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
+
+-- indent 
+o.autoindent = true
+o.smartindent = true
+
+-- search 
+o.hlsearch = true
+o.incsearch = true
+o.expandtab = true
+
+o.mouse = 'a'
+o.clipboard = 'unnamedplus'
+o.completeopt = 'menu,noinsert,noselect'
+o.showmatch = true
+o.foldmethod = 'marker'
+o.splitright = true
+o.splitbelow = true
+o.linebreak = true
+o.termguicolors = true
+o.laststatus = 3
+
+o.hidden = true o.history = 100
+o.lazyredraw = true
+o.updatetime = 250
+o.shiftround = true
+
+--disable some unnecessary plugin
+local disabled_built_ins = {
+   "2html_plugin",
+   "getscript",
+   "getscriptPlugin",
+   "gzip",
+   "logipat",
+   "netrw",
+   "netrwPlugin",
+   "netrwSettings",
+   "netrwFileHandlers",
+   "matchit",
+   "tar",
+   "tarPlugin",
+   "rrhelper",
+   "spellfile_plugin",
+   "vimball",
+   "vimballPlugin",
+   "zip",
+   "zipPlugin",
+   "tutor",
+   "rplugin",
+   "synmenu",
+   "optwin",
+   "compiler",
+   "bugreport",
+   "ftplugin",
+}  
+for _, plugin in pairs(disabled_built_ins) do 
+    global["loaded_" .. plugin] = 1
+end
